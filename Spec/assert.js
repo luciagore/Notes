8e63589e028c740375c8bb177723592076e8b279
luciagore <lucy.gore@hotmail.co.uk>
2018-06-26 12:00:27 +0100

    added assert isIncluded

--- Spec/assert.js
+++ Spec/assert.js
@@ -3,5 +3,10 @@ var assert = {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
     }
+  },
+  isIncluded: function(note, notelist) {
+    if(!notelist.includes(note)){
+      throw new Error("Assertion failed: " + x + " is not truthy");
+    }
   }
 };
--- Spec/note_list_spec.js
+++ Spec/note_list_spec.js
@@ -1,9 +1,8 @@
 (function(exports){
   function testNoteList() {
     var notelist = new NoteList();
-    var note = new Note("hello my name is Lucy");
-    notelist.addNote(note)
-    assert.isTrue(notelist.viewNotes() == [note])
+    notelist.addNote("note")
+    assert.isIncluded("note", notelist.viewNotes())
 
   };
   testNoteList();
